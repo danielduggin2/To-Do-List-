656d80206acf22d13cc8144f6e0297d67c338d6b
Daniel Duggin <46377670+danielduggin2@users.noreply.github.com>
2023-02-06 21:04:36 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Daniel Wade Duggin on 2/6/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
